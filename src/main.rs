1521340185b98c5fdea6433c7b2a0dd10ad98518
Paolo <str237@gmail.com>
2021-02-21 13:11:41 +0100

    Move initialization logic to init()

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 use std::io;
 use std::io::prelude::*;
 
-fn main() {
-    let stdin = io::stdin();
-    let mut i = 0_usize;
-    for line in stdin.lock().lines() {
-        println!("{}", line.unwrap());
+#[derive(Default)]
+struct Game {
+    m: usize,
+    t: (usize, usize, usize),
+}
+
+impl Game {
+    pub fn init() {
+        let mut instance = Self::default();
+
+        let stdin = io::stdin();
+        let mut line_iter = stdin.lock().lines();
+        let x = line_iter.next().unwrap().unwrap();
+        let first_line: Vec<&str> = x.split(' ').collect();
+        instance.m = (*first_line[0]).parse().unwrap();
+        instance.t = (
+            (*first_line[1]).parse().unwrap(),
+            (*first_line[2]).parse().unwrap(),
+            (*first_line[3]).parse().unwrap()
+        );
+        instance
     }
+}
+
+fn main() {
+    let game = Game::init();
 }
