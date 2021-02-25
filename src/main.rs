676325304a3cca5e6789297cdf82e6df56000cc2
davidivad96 <davidivad96@gmail.com>
2021-02-25 19:22:49 +0100

    Reading first line.

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ use std::{
 
 #[derive(Default, Debug)]
 struct Game {
-    m: usize,
-    t: (usize, usize, usize),
-    i: Vec<Vec<String>>,
-    s: Vec<Vec<i32>>,
+    d: usize,
+    i: usize,
+    s: usize,
+    v: usize,
+    f: usize,
+    solution: Vec<Vec<String>>,
 }
 
 impl Game {
@@ -24,30 +26,18 @@ impl Game {
             .split(' ')
             .map(|x| String::from(x))
             .collect();
-        instance.m = first_line[0].parse().unwrap();
-        instance.t = (
-            first_line[1].parse().unwrap(),
-            first_line[2].parse().unwrap(),
-            first_line[3].parse().unwrap(),
-        );
-        instance.i = vec![];
-        for line in line_iter {
-            let current_line: Vec<String> =
-                line.unwrap().split(' ').map(|x| String::from(x)).collect();
-            instance.i.push(current_line[1..].to_vec());
-        }
+        instance.d = first_line[0].parse().unwrap();
+        instance.i = first_line[1].parse().unwrap();
+        instance.s = first_line[2].parse().unwrap();
+        instance.v = first_line[3].parse().unwrap();
+        instance.f = first_line[4].parse().unwrap();
         instance
-    }
-
-    pub fn calculate_dummy_solution(&mut self) {
-        self.s = vec![vec![2], vec![2, 1, 4], vec![3, 0, 2, 3]];
     }
 
     pub fn write_solution(&self, path: String) {
         let mut file = LineWriter::new(File::create(path).unwrap());
-        for row in &self.s {
-            let parsed_row: Vec<String> = row.iter().map(|x| x.to_string()).collect();
-            file.write_all(parsed_row.join(" ").as_bytes()).unwrap();
+        for row in &self.solution {
+            file.write_all(row.join(" ").as_bytes()).unwrap();
             file.write_all(b"\n").unwrap();
         }
         file.flush().unwrap();
@@ -55,10 +45,9 @@ impl Game {
 }
 
 fn main() {
-    let mut game = Game::init();
+    let game = Game::init();
     println!("{:?}", &game);
-    game.calculate_dummy_solution();
-    let output_path = "a_example.out";
+    let output_path = "a.out";
     game.write_solution(String::from(output_path));
     println!("\nSolution written at: {}", output_path);
 }
